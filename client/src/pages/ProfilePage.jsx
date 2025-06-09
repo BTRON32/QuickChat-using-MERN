b251128eb71be5323e99379dfc49150f7bc8ddf8
BTRON32 <131682089+BTRON32@users.noreply.github.com>
2025-06-09 21:35:39 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -52,9 +52,9 @@ const handleSubmit=async(e)=>{
          type="text" required placeholder='Your name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' />
          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} required placeholder='Write profile bio' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
         <button type='submit' className='mb-9 bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
-        
+        <button onClick={cancel} className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Cancel</button>
       </form>
-      <button onClick={cancel} className='w-3/6 mr-200 max-w-xl backdrop-blur-2xl border-2 border-gray-600 max-sm:flex-col-reverse absolute mt-115 ml-10 mb-17 bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Cancel</button>
+      
       <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`} src={authUser?.profilePic || assets.logo_icon} alt="" />
     </div>
 
